de16ec7c8fdf3b34469cc48f5d74fdf8d0b4eb11
It24101952 <it24101952@my.sliit.lk>
2026-05-01 18:45:14 +0530

    added bottom nav bar and added admin accounts and dashboards and fixed for admin login and add UI for book the packages for the user,for admin package crud

--- 1. UserAuthentication - FrontEnd/src/api/axiosInstance.js
+++ 1. UserAuthentication - FrontEnd/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken, getRefreshToken, saveTokens, removeTokens } from '../utils/storage';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.2:8080/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
